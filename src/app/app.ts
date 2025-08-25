ecc3e3695aa4be4f26dd0eae720fbd26318414d1
TruongLeDuy123 <20522083@gm.uit.edu.vn>
2025-08-25 10:47:53 +0700

    feat: add logout feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { AuthService } from './core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, NzLayoutModule, NzMenuModule],
-  templateUrl: './app.html',
-  styleUrl: './app.less'
+    selector: 'app-root',
+    imports: [RouterOutlet, NzLayoutModule, NzMenuModule, CommonModule],
+    templateUrl: './app.html',
+    styleUrl: './app.less'
 })
-export class App {}
+export class App {
+    constructor(readonly auth: AuthService) { }
+
+    logout() {
+        this.auth.logout()
+    }
+}
